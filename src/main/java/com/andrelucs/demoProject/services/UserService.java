389a56a2a68ee8454532b6a78bb4b0c9ab9b29a7
Andre-lucs <andreltd312@gmail.com>
2023-08-23 20:17:00 -0300

    User findById NotFound exception handler

--- src/main/java/com/andrelucs/demoProject/services/UserService.java
+++ src/main/java/com/andrelucs/demoProject/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import com.andrelucs.demoProject.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,7 +23,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> u = repository.findById(id);
-        return u.get();
+        return u.orElseThrow(()-> new ResourceNotFoundException(id));
     }
 
     public List<String> findAllPhones() {
