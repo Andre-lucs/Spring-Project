1cd5eaf3c55258978fd66c0000c6dad8b3f785dd
Andre-lucs <andreltd312@gmail.com>
2023-08-23 20:53:45 -0300

    User delete DatabaseError exception handler

---
+++ src/main/java/com/andrelucs/demoProject/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.andrelucs.demoProject.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+    private static final long serialVersionUID = 1l;
+    public DatabaseException(String msg){
+        super(msg);
+    }
+}
