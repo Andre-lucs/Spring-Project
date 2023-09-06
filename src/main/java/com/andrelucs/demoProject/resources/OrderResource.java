845903ee584df165781cc84b4079519402904d87
Andre-lucs <andreltd312@gmail.com>
2023-09-05 23:19:57 -0300

    Created the following requests for orders: insert(POST), addItems(PUT), insertPayment(PUT), updateStatus(PUT), increaseStatusLevel(PUT).

--- src/main/java/com/andrelucs/demoProject/resources/OrderResource.java
+++ src/main/java/com/andrelucs/demoProject/resources/OrderResource.java
@@ -1,15 +1,17 @@
 package com.andrelucs.demoProject.resources;
 
 import com.andrelucs.demoProject.entities.Order;
+import com.andrelucs.demoProject.entities.OrderItem;
+import com.andrelucs.demoProject.entities.Payment;
 import com.andrelucs.demoProject.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping(value="/orders")
@@ -24,9 +26,42 @@ public class OrderResource {
 		return ResponseEntity.ok().body(list);
 	}
 
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<Order> findById( @PathVariable Long id){
+	@GetMapping(value = "/")
+	public ResponseEntity<Order> findById( @RequestParam("id") Long id){
 		Order u = service.findById(id);
 		return ResponseEntity.ok().body(u);
+	}
+
+	@PostMapping
+	public ResponseEntity<Order> insert(@RequestBody Order obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(obj.getId())
+            .toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@PutMapping(value = "/items/")
+	public ResponseEntity<Order> addItems(@RequestParam("id") Long id, @RequestBody Set<OrderItem> orderItems){
+		Order response = service.addItems(id, orderItems);
+		return ResponseEntity.ok().body(response);
+	}
+    @PutMapping(value = "/payment/")
+    public ResponseEntity<Order> insertPayment(@RequestParam("id") Long id,@RequestBody Payment payment){
+        Order response = service.insertPayment(id, payment);
+        return ResponseEntity.ok().body(response);
+    }
+
+	@PutMapping(value = "/status/")
+	public ResponseEntity<Order> updateStatus(@RequestParam("id")Long id, @RequestParam("status") String newStatus){
+        Order response = service.updateStatus(id, newStatus);
+		return ResponseEntity.ok().body(response);
+	}
+	
+	@PutMapping(value = "/increase-status-level/")
+	public ResponseEntity<Order> increaseStatusLevel(@RequestParam("id") Long id){
+		Order response = service.increaseStatusLevel(id);
+		return ResponseEntity.ok().body(response);
 	}
 }
--- src/main/java/com/andrelucs/demoProject/services/OrderService.java
+++ src/main/java/com/andrelucs/demoProject/services/OrderService.java
@@ -1,27 +1,101 @@
 package com.andrelucs.demoProject.services;
 
-import com.andrelucs.demoProject.entities.Order;
+import com.andrelucs.demoProject.entities.*;
+import com.andrelucs.demoProject.entities.enums.OrderStatus;
+import com.andrelucs.demoProject.repositories.OrderItemRepository;
 import com.andrelucs.demoProject.repositories.OrderRepository;
+import com.andrelucs.demoProject.repositories.ProductRepository;
+import com.andrelucs.demoProject.repositories.UserRepository;
+import com.andrelucs.demoProject.services.exceptions.ConflictException;
+import com.andrelucs.demoProject.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class OrderService {
     
     @Autowired
-    private OrderRepository repository;
+    private OrderRepository orderRepository;
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     public List<Order> findAll(){
-        return repository.findAll();
+        return orderRepository.findAll();
     }
 
     public Order findById(Long id){
-        Optional<Order> u = repository.findById(id);
-        return u.get();
+        Optional<Order> u = orderRepository.findById(id);
+        return u.orElseThrow(()->new ResourceNotFoundException(id));
     }
 
+    public Order insert(Order obj){
+        if(obj.getPayment() == null) obj.setOrderStatus(OrderStatus.WAITING_PAYMENT);
+        User u = userRepository.findById(obj.getClient().getId())
+                .orElseThrow(()->new ResourceNotFoundException(obj.getClient().getId(), "User"));
+        obj.setClient(u);
+        Order order = orderRepository.save(obj);
+        addItems(order.getId(), obj.getItems());
+        return order;
+    }
+
+    public Order addItems(Long orderId, Set<OrderItem> items){
+        Order order = orderRepository.findById(orderId).orElseThrow(()->new ResourceNotFoundException(orderId));
+        for(OrderItem oi : items){
+            Product p = productRepository.findById(oi.getProduct().getId())
+                    .orElseThrow(()->new ResourceNotFoundException(oi.getProduct().getId(), "Product"));
+
+            if(oi.getValue() == null){
+                oi.setValue(p.getPrice());
+            }
+            oi.setProduct(p);
+            oi.setOrder(order);
+
+            order.getItems().add(oi);
+        }
+        orderItemRepository.saveAll(order.getItems());
+        return order;
+    }
+
+    public Order insertPayment(Long orderId, Payment payment) {
+        try {
+            Order order = orderRepository.findById(orderId).orElseThrow(() -> new ResourceNotFoundException(orderId));
+            if(order.getPayment() != null) throw new ConflictException(orderId);
+            order.setPayment(payment);
+            payment.setOrder(order);
+            if(order.getOrderStatus() == OrderStatus.WAITING_PAYMENT)
+                order.setOrderStatus(OrderStatus.PAID);
+            return orderRepository.save(order);
+        }catch(DataIntegrityViolationException e){
+            throw new ConflictException(orderId);
+        }
+    }
+
+    public Order updateStatus(Long orderId, String newStatus){
+        OrderStatus s;
+        try {
+            Integer code = Integer.parseInt(newStatus);
+            s = OrderStatus.valueByCode(code).orElse(null);
+        } catch (NumberFormatException e){
+            s = OrderStatus.valueByName(newStatus).orElse(null);
+        }
+        if(s == null) throw new ResourceNotFoundException(orderId, "OrderStatus");
+        Order o = orderRepository.getReferenceById(orderId);
+        o.setOrderStatus(s);
+        return orderRepository.save(o);
+    }
+
+	public Order increaseStatusLevel(Long id) {
+		Order o = findById(id);
+		o.setOrderStatus(OrderStatus.valueByCode(o.getOrderStatus().getCode()+1).orElse(OrderStatus.DELIVERED));
+		return orderRepository.save(o);
+	}
 }
